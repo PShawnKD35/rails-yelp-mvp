7d6812f52e0f1cb556484ba22633ef12d3133db9
Shawn Peng <shawn-peng@outlook.com>
2019-07-24 15:30:07 +0800

    added index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
-  get 'reviews/new'
-  get 'restaurants/index'
-  get 'restaurants/show'
-  get 'restaurants/new'
+  root to: "restaurants#index"
+  resources :restaurants, only: [:index, :new, :show] do
+    resources :reviews, only: [:new]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
